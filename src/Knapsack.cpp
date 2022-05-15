95956acade922533baf5d7c9939931b8ea18d00c
BbqGamer <korba.adam@gmail.com>
2022-05-15 21:51:26 +0200

    Add Knapsack basic interfaces

---
+++ src/Knapsack.cpp
@@ -0,0 +1,28 @@
+#include "Knapsack.h"
+
+ProblemInstance generateRandomProblemInstance(int n, int C) {
+    ProblemInstance instance;
+    instance.C = C;
+    for (int i = 0; i < n; i++) {
+        Item item;
+        item.w = rand() % int(MAX_W_TO_C_RATIO * C) + 1;
+        item.v = rand() % MAX_VALUE + 1;
+        instance.items.push_back(item);
+        instance.solution.push_back(0);
+    }
+    return instance;
+}
+
+std::ostream& operator<<(std::ostream& os, const Item& item) {
+    os << "(" << item.w << ", " << item.v << ")";
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const ProblemInstance& instance) {
+    os << "C: " << instance.C << std::endl;
+    os << "Items: " << std::endl;
+    for (int i = 0; i < instance.size(); i++) {
+        os << instance.items[i] << std::endl;
+    }
+    return os;
+}
---
+++ src/Knapsack.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vector>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+#define MAX_VALUE 100
+#define MAX_W_TO_C_RATIO 0.3
+
+struct Item {
+    int w;
+    int v;
+};
+
+std::ostream& operator<<(std::ostream& os, const Item& item);
+
+
+class ProblemInstance {
+    public:
+    int C;
+    std::vector<Item> items;
+    
+    std::vector<bool> solution;
+
+    ProblemInstance() {
+        C = 0;
+    }
+    
+    int size() const {
+        return items.size();
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const ProblemInstance& instance);
+
+ProblemInstance generateRandomProblemInstance(int n, int C);
